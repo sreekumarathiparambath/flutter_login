319e00b030e62bc7803c0db0c3a0960be8ac7b1a
Sreekumar Athiparambath <sreekumaralan@gmail.com>
2020-02-03 11:43:15 +0530

    first commit

---
+++ lib/blocs/bloc.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+import 'validators.dart';
+import 'package:rxdart/rxdart.dart';
+
+class Bloc extends Object with Validators {
+  final _email = StreamController<String>.broadcast();
+  final _password = StreamController<String>.broadcast();
+
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream.transform(validatePassword);
+  Stream<bool> get submitValid =>
+      Observable.combineLatest2(email, password, (e, p) => true);
+
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
+
+  dispose() {
+    _email.close();
+    _password.close();
+  }
+}
